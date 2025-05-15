ef5f67ab1fae11d857354c91c876ff6e699cbcb9
mislavivanda <mislavivanda454@gmail.com>
2025-05-15 15:34:47 +0200

    Dashboard overview devices and data types cards data update

--- src/features/overview/components/overview-cards.jsx
+++ src/features/overview/components/overview-cards.jsx
@@ -10,7 +10,8 @@ import {
   CardFooter
 } from '@/components/ui/card';
 import { IconTrendingUp } from '@tabler/icons-react';
-import { tokensBalance, totalTokensSum } from '@/constants/data';
+import { tokensBalance, totalTokensSum,   devices,
+  availableDataTypesFilterOptions } from '@/constants/data';
 export function OverviewCards() {
   return (
     <>
@@ -43,7 +44,7 @@ export function OverviewCards() {
         <CardHeader>
           <CardDescription>Active Devices</CardDescription>
           <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
-            5
+            {devices.length}
           </CardTitle>
           <CardAction></CardAction>
         </CardHeader>
@@ -57,7 +58,7 @@ export function OverviewCards() {
         <CardHeader>
           <CardDescription>Data Types</CardDescription>
           <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
-            4
+            {availableDataTypesFilterOptions.length}
           </CardTitle>
         </CardHeader>
         <CardFooter className='flex-col items-start gap-1.5 text-sm'>
